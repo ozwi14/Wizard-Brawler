cae8a435e8b139cb7b1f0c5fe09bc2cee49c1e5f
DylanBrazendale <DJBRAZENDALE@pm.me>
2026-01-30 23:00:03 +1300

    feat: add player ready and device ready checks

--- Levels/UI/player_selection_box.gd
+++ Levels/UI/player_selection_box.gd
@@ -3,30 +3,38 @@ class_name PlayerSelectionBox extends Control
 var player_id : int
 var device_id : int
 
-var looking_for_player : bool = false
-var player_found : bool = false
+var looking_for_device : bool = false
+var waiting_for_player_ready : bool = false
 
 var player_is_ready: bool = false
 
+var ready_time_required : float = 3.0
+var ready_time_held : float = 0.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if player_is_ready:
 		return
 	
-	if looking_for_player:
-		_check_for_player()
-		return
-	
-	if player_found:
-		_check_for_ready()
+	if waiting_for_player_ready:
+		_check_for_ready(delta)
 		return
 
 func _input(event):
-	if event.is_action_pressed("ui_start"):
-		print("Start pressed by controller:", event.device)
+	if looking_for_device:
+		if event.is_action_pressed("ui_accept"):
+			print("Start pressed by controller:", event.device)
+			device_id = event.device
+			looking_for_device = false
+			waiting_for_player_ready = true
 
-func _check_for_player() -> void:
-	pass
-
-func _check_for_ready() -> void:
-	pass
+func _check_for_ready(delta: float) -> void:
+	if Input.is_joy_button_pressed(device_id, JOY_BUTTON_START):
+		ready_time_held += delta
+		
+		if ready_time_held >= ready_time_required:
+			waiting_for_player_ready = false
+			player_is_ready = true
+	else:
+		ready_time_held = 0.0
+	
