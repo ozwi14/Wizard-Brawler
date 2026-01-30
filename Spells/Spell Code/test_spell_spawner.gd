584cc9e3f720594fd99157d97b9451bb581e5771
ozwi14 <60504535+ozwi14@users.noreply.github.com>
2026-01-30 22:49:44 +1300

    spell rotation
    Added spell spawn rotator and corrected spell movement to be rotated based on spawn

--- Spells/Spell Code/test_spell_spawner.gd
+++ Spells/Spell Code/test_spell_spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var child_spell : PackedScene
+@export var direction : Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	call_deferred("spawn_children")
@@ -10,6 +11,8 @@ func spawn_children():
 	if child_spell == null :
 		return
 	var subspell = child_spell.instantiate()
+	subspell.global_position = global_position
+	subspell.global_rotation = global_rotation
 	get_parent().add_child(subspell)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
