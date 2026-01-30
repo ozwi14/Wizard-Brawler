182cfe80312d863d7c912f29a9146c1dc635b051
DylanBrazendale <DJBRAZENDALE@pm.me>
2026-01-31 10:39:43 +1300

    feat: Devices linked to players

--- Levels/UI/player_selection_box.gd
+++ Levels/UI/player_selection_box.gd
@@ -3,38 +3,52 @@ class_name PlayerSelectionBox extends Control
 var player_id : int
 var device_id : int
 
-var looking_for_device : bool = false
-var waiting_for_player_ready : bool = false
+enum PlayerSelectionState {
+	Disabled,
+	LookingForDevice,
+	DeviceFound,
+	PlayerReady,
+}
 
-var player_is_ready: bool = false
+@export var player_state : PlayerSelectionState = PlayerSelectionState.Disabled
 
-var ready_time_required : float = 3.0
+@export var ready_time_required : float = 0.5
 var ready_time_held : float = 0.0
+
+func _ready() -> void:
+	var devices = Input.get_connected_joypads()
+	print("Device Ids: ", devices)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player_is_ready:
+	if player_state == PlayerSelectionState.PlayerReady:
 		return
 	
-	if waiting_for_player_ready:
+	if player_state == PlayerSelectionState.DeviceFound:
 		_check_for_ready(delta)
 		return
 
 func _input(event):
-	if looking_for_device:
-		if event.is_action_pressed("ui_accept"):
-			print("Start pressed by controller:", event.device)
+	#print("Controller %d pressed a button", event.device)
+	if player_state == PlayerSelectionState.LookingForDevice:
+		var local_device_id = event.device
+		if GameManager.devices_mapped.find(local_device_id) != -1:
+			return
+		
+		if event.is_action_pressed("action_start"):
+			print("Start pressed by controller:", local_device_id)
 			device_id = event.device
-			looking_for_device = false
-			waiting_for_player_ready = true
+			player_state = PlayerSelectionState.DeviceFound
+			GameManager.devices_mapped.append(device_id)
 
 func _check_for_ready(delta: float) -> void:
 	if Input.is_joy_button_pressed(device_id, JOY_BUTTON_START):
 		ready_time_held += delta
 		
+		print("Time held: ", ready_time_held)
+		
 		if ready_time_held >= ready_time_required:
-			waiting_for_player_ready = false
-			player_is_ready = true
+			player_state = PlayerSelectionState.PlayerReady
 	else:
 		ready_time_held = 0.0
 	
--- Levels/player_join_menu.gd
+++ Levels/player_join_menu.gd
@@ -6,30 +6,51 @@ extends Control
 
 @export var player_boxes: Array[PlayerSelectionBox]
 
+var all_players_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player_boxes[0].player_state = PlayerSelectionBox.PlayerSelectionState.LookingForDevice
 	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var players_ready : bool = _are_all_players_ready();
-	if players_ready:
+	if all_players_ready:
+		print("All Players ready, load next Scene")
 		# Store player data
 		# Load next scene
 		return
+		
+	all_players_ready = _are_all_players_ready();
 	
+	_allow_waiting_for_next_player()
 	
 
 func _allow_waiting_for_next_player() -> void:
+	var enabled_next_player_selection : bool = false
 	for player_box in player_boxes:
-		if not player_box.enabled:
-			player_box.enabled = true
+		if player_box.player_state == player_box.PlayerSelectionState.DeviceFound:
+			enabled_next_player_selection = true
+			continue
+		
+		if enabled_next_player_selection:
+			player_box.player_state = player_box.PlayerSelectionState.LookingForDevice
+			return
 	pass
 
+# Think about replacing this with signals
 func _are_all_players_ready() -> bool:
+	var players_ready: int = 0
 	for player_box in player_boxes:
-		if player_box.enabled and not player_box.player_is_ready:
+		if player_box.player_state == PlayerSelectionBox.PlayerSelectionState.Disabled:
+			print("Player disabled skip")
+			continue
+		if player_box.player_state == PlayerSelectionBox.PlayerSelectionState.DeviceFound:
+			print("Player found, but not ready")
 			return false
-	return true
+		if player_box.player_state == PlayerSelectionBox.PlayerSelectionState.PlayerReady:
+			players_ready += 1
+	
+	return players_ready > 0
