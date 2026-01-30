fe373ba9b895ca8cc5d209c21dfba154df5eb8d9
Hamsterhill <Binshill@gmail.com>
2026-01-31 00:51:16 +1300

    Playable Movement v1
    2 player objects (one for testing) added, adjusted player size, world gravity, collisions and movement speeds for peppy movement

--- Player/Player 2 FOR TESTING/player 2.gd
+++ Player/Player 2 FOR TESTING/player 2.gd
@@ -8,19 +8,22 @@ func _ready() -> void:
 	SPEED = get_parent().SPEED
 	JUMP_VELOCITY = get_parent().JUMP_VELOCITY
 
+
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("p1 jump") and is_on_floor():
+	if Input.is_action_just_pressed("p2 jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
-		print("p1 Jump")
+		print("p2 Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p1 left", "p1 right")
+	var direction := Input.get_axis("p2 left", "p2 right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
