19d453dd054ada5b8e83bcd6ecd483cbf3b656be
Kyoubou <kyoubouhill@gmail.com>
2026-01-31 10:19:05 +1300

    Movement indicator
    adds arrow that shows movement indicator and save last direction looked at

--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 var SPEED : float
 var JUMP_VELOCITY : float
 var CAN_DOUBLEJUMP = 0
+var FIRE_ANGLE : Vector2
+var end_pos : Vector2 = Vector2(150, 150)
+var line_color : Color = Color.BLACK
+var line_thickness : float = 30.0
 
 func _ready() -> void:
 	SPEED = get_parent().SPEED
@@ -30,5 +34,18 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	#Determing the direction the player is intending to travle and fire
+	FIRE_ANGLE = Vector2(Input.get_vector("p1 left", "p1 right", "p1 up", "p1 down")*1000)
+	#print(FIRE_ANGLE)
+	
 	move_and_slide()
+	queue_redraw()
+	
+	
+	
+	
+#func _draw():
+	draw_line(Vector2(0,0),FIRE_ANGLE,line_color,line_thickness)
+	
+	
