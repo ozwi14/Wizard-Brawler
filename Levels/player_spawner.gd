7160d730df14ce7a8a6cbf76cf3f9e7fd8571569
DylanBrazendale <DJBRAZENDALE@pm.me>
2026-01-31 14:38:13 +1300

    Added debug spawner to player spawner

--- Levels/player_spawner.gd
+++ Levels/player_spawner.gd
@@ -3,13 +3,29 @@ class_name PlayerSpawner extends Node2D
 @export var spawn_points: Array[Node2D]
 @export var player_scene: PackedScene
 
+@export var debug_spawn_players : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if debug_spawn_players:
+		return
 	assert(spawn_points.size() >= GameManager.players.size())
 	pass # Replace with function body.
 
 func spawn_players() -> void:
 	print("Spawnning Players")
+	if debug_spawn_players:
+		print("Debug spawning players with fake data")
+		GameManager.players.clear()
+		var player1 := PlayerData.new()
+		var player2 := PlayerData.new()
+		player1.device_id = 0
+		player1.player_id = 1
+		player2.device_id = 1
+		player2.player_id = 2
+		GameManager.players.append(player1)
+		GameManager.players.append(player2)
+	
 	var index = 0
 	for player_data in GameManager.players:
 		var player: Player = player_scene.instantiate()
