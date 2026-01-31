b824e17d1129e4d51e24529166e4167d7ab178e9
ozwi14 <ozwi.andrew@gmail.com>
2026-01-31 14:31:17 +1300

    Added input and spell array
    Added input and spell array

--- Player/Player 2 FOR TESTING/player 2.gd
+++ Player/Player 2 FOR TESTING/player 2.gd
@@ -1,7 +1,10 @@
-extends "res://Player/player.gd"
+extends Player
 
 func _ready() -> void:
 	#setting controls
 	key_jump = "p2 jump"
 	key_left = "p2 left"
 	key_right = "p2 right"
+	key_spell_x = "p2 x"
+	key_spell_y = "p2 y"
+	key_spell_b = "p2 b"
